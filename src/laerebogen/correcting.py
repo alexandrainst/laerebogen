d4323cd1d3f51e6bad1081f85de31cd7ddaacbb3
saattrupdan <saattrupdan@gmail.com>
2025-07-29 13:30:52 +0200

    feat: Add ability to correct dataset

---
+++ src/laerebogen/correcting.py
@@ -0,0 +1,92 @@
+"""Correcting spelling mistakes in text."""
+
+from copy import deepcopy
+
+from .data_models import InstructionSample
+from .vllm_utils import generate_text_with_vllm, load_vllm_model
+
+CORRECTION_PROMPT_TEMPLATE = (
+    "Skriv følgende #Original Tekst# om, hvor du retter all stavefejl og "
+    "grammatiske fejl. Du må kun skrive på dansk, du må ikke ændre betydningen af "
+    "instruktionen, og du skal kun skrive teksten uden yderligere tekst. Du skal "
+    "skrive den rettede tekst under #Rettet Tekst#. Hvis teksten er '<empty>', så "
+    "skal du også skrive '<empty>' under #Rettet Tekst#.\n\n"
+    "#Original Tekst#:\n"
+    "{text}\n\n"
+    "#Rettet Tekst#:\n"
+)
+
+
+def correct_instructions(
+    instructions: list[InstructionSample], model_id: str
+) -> list[InstructionSample]:
+    """Correct spelling mistakes using an instruction-tuned large language model.
+
+    Args:
+        instructions:
+            The instructions to correct.
+        model_id:
+            The model ID of the instruction-tuned large language model to use for
+            correction.
+
+    Returns:
+        The corrected instructions.
+    """
+    # Load the model
+    model = load_vllm_model(model_id=model_id)
+
+    # Copy the instructions to avoid modifying the original ones
+    corrected_instructions = deepcopy(instructions)
+
+    # Correct the instructions
+    prompts = [
+        CORRECTION_PROMPT_TEMPLATE.format(
+            text=instruction.instruction if instruction.instruction else "<empty>"
+        )
+        for instruction in instructions
+    ]
+    prompts = model.tokenizer.apply_chat_template(prompts, add_generation_prompt=True)
+    responses = generate_text_with_vllm(prompts=prompts, model=model)
+    for instruction, response in zip(corrected_instructions, responses):
+        if response.done_reason == "stop":
+            instruction.instruction = (
+                response.completion.strip()
+                if response.completion.strip() != "<empty>"
+                else ""
+            )
+
+    # Correct the inputs
+    prompts = [
+        CORRECTION_PROMPT_TEMPLATE.format(
+            text=instruction.input if instruction.input else "<empty>"
+        )
+        for instruction in instructions
+    ]
+    prompts = model.tokenizer.apply_chat_template(prompts, add_generation_prompt=True)
+    responses = generate_text_with_vllm(prompts=prompts, model=model)
+    for instruction, response in zip(corrected_instructions, responses):
+        if response.done_reason == "stop":
+            instruction.input = (
+                response.completion.strip()
+                if response.completion.strip() != "<empty>"
+                else ""
+            )
+
+    # Correct the outputs
+    prompts = [
+        CORRECTION_PROMPT_TEMPLATE.format(
+            text=instruction.output if instruction.output else "<empty>"
+        )
+        for instruction in instructions
+    ]
+    prompts = model.tokenizer.apply_chat_template(prompts, add_generation_prompt=True)
+    responses = generate_text_with_vllm(prompts=prompts, model=model)
+    for instruction, response in zip(corrected_instructions, responses):
+        if response.done_reason == "stop":
+            instruction.output = (
+                response.completion.strip()
+                if response.completion.strip() != "<empty>"
+                else ""
+            )
+
+    return corrected_instructions
---
+++ src/scripts/correct_dataset.py
@@ -0,0 +1,79 @@
+"""Evolve the generated instruction-following dataset.
+
+Usage:
+    python correct_dataset.py \
+        [--dataset-path <dataset_path>] \
+        [--model <model>] \
+        [--verbose]
+"""
+
+import logging
+from pathlib import Path
+
+import click
+
+from laerebogen.correcting import correct_instructions
+from laerebogen.data_models import InstructionSample
+
+
+@click.command()
+@click.option(
+    "--dataset-path",
+    type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True),
+    default="data/dataset.jsonl",
+    show_default=True,
+    help="Path to the dataset file.",
+)
+@click.option(
+    "--model",
+    type=str,
+    default="google/gemma-3-27b-it",
+    show_default=True,
+    help="Model ID of the instruction-tuned large language model to use for evolution.",
+)
+@click.option(
+    "--verbose",
+    is_flag=True,
+    default=False,
+    show_default=True,
+    help="Enable verbose logging.",
+)
+def evolve(dataset_path: str | Path, model: str, verbose: bool) -> None:
+    """Evolve the instruction-following dataset.
+
+    Args:
+        dataset_path:
+            Path to the dataset file.
+        model:
+            Model ID of the instruction-tuned large language model to use for evolution.
+        verbose:
+            Enable verbose logging.
+
+    Raises:
+        FileNotFoundError:
+            If the dataset file does not exist.
+    """
+    logging.basicConfig(
+        level=logging.DEBUG if verbose else logging.INFO,
+        format="%(asctime)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    # Load the dataset
+    dataset_path = Path(dataset_path)
+    if not dataset_path.exists():
+        raise FileNotFoundError(f"Dataset file not found: {dataset_path!r}")
+    instructions = [
+        InstructionSample.from_json(line.strip())
+        for line in Path(dataset_path).read_text().splitlines()
+    ]
+
+    # Correct the dataset
+    instructions = correct_instructions(instructions=instructions, model_id=model)
+    with dataset_path.with_suffix(".corrected.jsonl").open("w", encoding="utf-8") as f:
+        for instruction in instructions:
+            f.write(instruction.json() + "\n")
+
+
+if __name__ == "__main__":
+    evolve()
