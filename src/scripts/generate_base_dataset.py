68b81756a94607805e6c911ea25212f620f7cb4f
saattrupdan <saattrupdan@gmail.com>
2026-01-29 23:01:21 +0100

    feat: Use Qwen for generation

--- src/scripts/generate_base_dataset.py
+++ src/scripts/generate_base_dataset.py
@@ -55,7 +55,7 @@ from laerebogen.generation import generate_instruction_following_data
 @click.option(
     "--model",
     type=str,
-    default="mistralai/Mistral-Small-3.1-24B-Base-2503",
+    default="Qwen/Qwen3-30B-A3B-Base",
     show_default=True,
     help="The model ID of the model to use for generation. Must be a base model, not "
     "a finetuned one.",
