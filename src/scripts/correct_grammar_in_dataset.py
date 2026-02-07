fc2b49044147fc4b5378b70318e5c4fff1d88e08
saattrupdan <saattrupdan@gmail.com>
2026-02-07 11:36:13 +0100

    feat: Change grammar correction model to Gemma-3-27b

--- src/scripts/correct_grammar_in_dataset.py
+++ src/scripts/correct_grammar_in_dataset.py
@@ -38,9 +38,10 @@ from laerebogen.data_models import InstructionSample
 @click.option(
     "--model",
     type=str,
-    default="deepcogito/cogito-v1-preview-llama-70B",
+    default="google/gemma-3-27b-it",
     show_default=True,
-    help="Model ID of the instruction-tuned large language model to use for evolution.",
+    help="Model ID of the instruction-tuned large language model to use for grammar "
+    "correction.",
 )
 @click.option(
     "--verbose",
