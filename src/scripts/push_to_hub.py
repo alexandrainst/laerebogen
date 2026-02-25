87de7edd6d87cafba7afb734e69b12fe22022f60
saattrupdan <saattrupdan@gmail.com>
2026-02-25 14:21:28 +0100

    feat: Make the push_to_hub.py script more robust

--- src/scripts/push_to_hub.py
+++ src/scripts/push_to_hub.py
@@ -1,10 +1,11 @@
 """Push the dataset to the Hugging Face Hub.
 
 Usage:
-    python push_to_hub.py [--data-path <data_path>] [--repo_id <repo_id>] [--public]
+    python push_to_hub.py JSONL_DATA_PATH REPO_ID[::SUBSET] [--public]
 """
 
 import logging
+from pathlib import Path
 
 import click
 from datasets import Dataset, load_dataset
@@ -16,30 +17,27 @@ logger = logging.getLogger("push_to_hub")
 
 
 @click.command()
-@click.option(
-    "--data_path",
-    type=click.Path(exists=True, file_okay=True, dir_okay=False, readable=True),
-    default="data/dataset.with_follow_ups.jsonl",
-    show_default=True,
-    help="Path to the dataset stored as a JSONL file.",
+@click.argument(
+    "data_path",
+    type=click.Path(exists=True, dir_okay=False, path_type=Path, readable=True),
+    help="The path to the JSONL dataset.",
 )
-@click.option(
-    "--repo_id",
+@click.argument(
+    "repo_id",
     type=str,
-    default="danish-foundation-models/laerebogen",
-    show_default=True,
-    help="The repository ID on the Hugging Face Hub where the dataset will be pushed.",
+    help="The repository ID on the Hugging Face Hub. Include the subset by separating "
+    "with double colons ('::'), e.g. 'username/repo::subset'.",
 )
 @click.option(
     "--public",
     is_flag=True,
     default=False,
-    help="Whether to create a public repository on the Hugging Face Hub.",
+    help="Push the dataset to the Hugging Face Hub as a public dataset.",
 )
-def main(data_path: str, repo_id: str, public: bool) -> None:
+def main(data_path: Path, repo_id: str, public: bool) -> None:
     """Push the dataset to the Hugging Face Hub."""
     logger.info(f"Loading dataset from {data_path}...")
-    dataset = load_dataset("json", data_files=data_path, split="train")
+    dataset = load_dataset("json", data_files=data_path.as_posix(), split="train")
     assert isinstance(dataset, Dataset)
     logger.info(f"Dataset loaded with {len(dataset):,} examples.")
 
@@ -50,7 +48,11 @@ def main(data_path: str, repo_id: str, public: bool) -> None:
     )
 
     logger.info(f"Pushing dataset to Hugging Face Hub at {repo_id!r}...")
-    dataset.push_to_hub(repo_id, private=not public)
+    if "::" in repo_id:
+        repo_id, subset = repo_id.split("::")
+    else:
+        pass
+    dataset.push_to_hub(repo_id, config_name="default", private=not public)
     logger.info(f"Dataset pushed to {repo_id!r} successfully.")
 
 
